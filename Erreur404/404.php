090613e197199ecf1f9b160f5068f74967f38b40
ImFormal <thubert.maxime.pro@gmail.com>
2024-12-03 11:53:56 +0100

    Ajout Connexion.php, Erreur404.php et Footer.php

---
+++ Erreur404/404.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8" />
+    <link rel="icon" type="image/svg" href="../Ressources/Logo/vulpiz_icon.svg" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="description" content="Découvrez VulpiZ, votre site de quiz innovant mélant culture, fun et renard !">
+    <link rel="stylesheet" href="../style.css">
+    <title>Erreur 404 | VulpiZ</title>
+</head>
+<body>
+    <div class="content">
+        <h1>Erreur 404 - Page non trouvée</h1>
+        <div class="logo">
+            <img src="../Ressources/404_fox.svg">
+        </div>
+        <p>La page que vous recherchez n'existe pas.</p>
+        <p><a href="../index.html">Retour à l'accueil</a></p>
+    </div>
+    <?php include '../Modèles/Footer/footer.php'; ?>
+</body>
+</html>
